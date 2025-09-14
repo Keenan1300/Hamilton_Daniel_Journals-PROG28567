1e39dad605be38396cb909b58a42e4a39f5183d7
Keenan1300 <hamilt25@sheridancollege.ca>
2025-09-14 18:59:59 -0400

    last minute fix for draw vectors

--- Assets/Scripts/DrawVectors.cs
+++ Assets/Scripts/DrawVectors.cs
@@ -5,23 +5,23 @@ using UnityEngine.Events;
 
 public class DrawVectors : MonoBehaviour
 {
-    Vector2 dVector;
-    Vector2 eVector;
+    public Vector2 dVector;
+    public Vector2 eVector;
 
     void Update()
     {
         // Create two Vector2 variables, "dVector" and "eVector"
         // Set their values to dVector: (0, 1) and eVector: (3, -2)
 
-        Vector2 dVector = new Vector2(0, 1);
-        Vector2 eVector = new Vector2(3, -2);
+        Vector2 dVector = new Vector2(0f, 1f);
+        Vector2 eVector = new Vector2(3f, -2f);
 
 
 
         // Use Debug.DrawLine to draw a yellow vector starting at the origin and ending at dVector.
         // Use Debug.DrawLine to draw a grey vector starting at the origin and ending at eVector.
-        Debug.DrawLine(Vector.zero, dVector,Color.yellow);
-        Debug.DrawLine(Vector.zero, eVector, Color.grey);
+        Debug.DrawLine(Vector2.zero, dVector,Color.yellow);
+        Debug.DrawLine(Vector2.zero, eVector, Color.grey);
         
     }
 }
