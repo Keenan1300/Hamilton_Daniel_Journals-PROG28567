9ee0f21eb776e6c8fd02a6c1a6848a667b1c7920
Keenan1300 <hamilt25@sheridancollege.ca>
2025-09-20 21:09:15 -0400

    Ran Out of time for task 2

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     public float BombTrailSpacing;
     public int NumberOfTrailBombs;
 
+    public float inDistance;
+
     // Update is called once per frame
     void Update()
     {
@@ -30,39 +32,86 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.B))
         {
 
-            SpawnBombAtOffset(new Vector3(0,1));
+            SpawnBombAtOffset(new Vector3(0, 1));
+
+        }
+
+
+        //SpawnCornerBomb
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SpawnBombAtRandomCorner(inDistance);
+
 
         }
     }
-        
+
     //Spawn Bomb at offset
     private void SpawnBombAtOffset(Vector3 inOffset)
-        {
-            Vector3 SpawnPosition = transform.position + inOffset;
-            Instantiate(bombPrefab, SpawnPosition, Quaternion.identity);
-        }
+    {
+        Vector3 SpawnPosition = transform.position + inOffset;
+        Instantiate(bombPrefab, SpawnPosition, Quaternion.identity);
+    }
 
     //Bomb trail task
-    public void SpawnBombTrail(float BombTrailSpacing, int NumberOfTrailBombs) 
-    {      
-        
+    public void SpawnBombTrail(float BombTrailSpacing, int NumberOfTrailBombs)
+    {
+
         float Spawnedbombs = 1;
 
-    for (int i = 0; i < NumberOfTrailBombs; i++) 
-       {
+        for (int i = 0; i < NumberOfTrailBombs; i++)
+        {
             Vector2 PlayerPosition = transform.position;
             Vector2 Bombposition = new Vector2(PlayerPosition.x, PlayerPosition.y + BombTrailSpacing * Spawnedbombs);
             Instantiate(bombPrefab, Bombposition, Quaternion.identity);
-            Spawnedbombs += 1; 
-     }
+            Spawnedbombs += 1;
+        }
+    }
 
 
 
     //Corner Bomb Exercise start
 
+    public void SpawnBombAtRandomCorner(float inDistance)
+    {
 
-}
-    
+        //choose random number from 1 to 4
+        int RandomCorner = Random.Range(0, 4);
 
-   
+        Vector2 Playerlocation = transform.position;
+
+        //Setup what "Corners" mean
+        Vector2 BottomL = new Vector2(Playerlocation.x - 1f, Playerlocation.y -1);
+        Vector2 BottomR = new Vector2(Playerlocation.x + 1f, Playerlocation.y - 1);
+        Vector2 TopR = new Vector2(Playerlocation.x + 1f, Playerlocation.y + 1);
+        Vector2 TopL = new Vector2(Playerlocation.x - 1f, Playerlocation.y + 1);
+
+        //Roll for what corner is chosen
+        if (RandomCorner == 1)
+        {
+            Vector2 NormalizeBottomL = BottomL.normalized;
+            Vector2 BombLocation = NormalizeBottomL * inDistance;
+            Instantiate(bombPrefab, BombLocation, Quaternion.identity);
+        }
+
+        if (RandomCorner == 2)
+        {
+            Vector2 NormalizeBottomR = BottomR.normalized;
+            Vector2 BombLocation = NormalizeBottomR * inDistance;
+            Instantiate(bombPrefab, BombLocation, Quaternion.identity);
+        }
+        if (RandomCorner == 3)
+        {
+            Vector2 NormalizeTopL = TopL.normalized;
+            Vector2 BombLocation = NormalizeTopL * inDistance;
+            Instantiate(bombPrefab, BombLocation, Quaternion.identity);
+        }
+        if (RandomCorner == 4)
+        {
+            Vector2 NormalizeTopR = TopR.normalized;
+            Vector2 BombLocation = NormalizeTopR * inDistance;
+            Instantiate(bombPrefab, BombLocation, Quaternion.identity);
+        }
+
+    }
 }
