d1d43a47228f4e63e7660673bd424a71e6e84757
Keenan1300 <hamilt25@sheridancollege.ca>
2025-09-28 22:48:50 -0400

    Asteroid tweak

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,50 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    //initiate random spot
+    public Vector3 RandomSpot;
+
     // Start is called before the first frame update
     void Start()
     {
+        //set random direction
+        RandomSpot = new Vector3(Random.Range(-20,20),Random.Range(-20,20),Random.Range(-20,20));
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        //velocity wont exceed this value.
+        velocity = Vector3.ClampMagnitude(RandomSpot, maxFloatDistance);
+
+        //Find direction between enemy and asteroid
+        Vector3 enemypos = transform.position;
+        Vector3 ChosenAsteroidPosition = ChosenAsteroid.position;
+        Vector3 DirectionMove = ChosenAsteroidPosition - enemypos;
+
+        //Calculate variables for velocity
+        float accelerationRate = MaxSpeed / Accelerationtime * 2;
+
+        //Set enemy motion towards asteroid
+        velocity += accelerationRate * Time.deltaTime * DirectionMove;
+
+        //set distance to check if enemy is nearby target asteroid
+        float targetmagnitude = DirectionMove.magnitude;
+
+        if (targetmagnitude < Maxrange)
+        {
+            reachedasteroid = true;
+        }
+
+
+   
+
+        Vector3 direction = (velocity).normalized;
+        transform.position += direction * Time.deltaTime;
+
     }
+
+
 }
