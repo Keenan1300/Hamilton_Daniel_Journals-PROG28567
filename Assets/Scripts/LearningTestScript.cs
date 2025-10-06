2869af2bed9c7aafa1b291899402c0e6080e31fe
hamilt25 <hamilt25@OA-S141-01>
2025-10-06 16:03:51 -0400

    In class activity. Important notes: do first proposal with vectors, addition, subtractions, magnitude, etc

--- Assets/Scripts/LearningTestScript.cs
+++ Assets/Scripts/LearningTestScript.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LearningTestScript : MonoBehaviour
 {
@@ -15,69 +17,97 @@ public class LearningTestScript : MonoBehaviour
     private int currentIndex = 0;
     private bool isRunning = false;
 
+    //dotproduct
+    public float RedAngleDeg = 60f;
+    public float BlueAngleDeg = 30f;
+
     public Vector3 cirlecenter = Vector3.zero;
 
-    public Transform Player;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentIndex = 0;
+        //currentIndex = 0;
 
         //calculate vertices
-        for (int i = 0; i < numberofAngles + 1; i++)
-        {
-            float floatconvert = i;
-            angles.Add(floatconvert / numberofAngles * 360f);
-        }
+       // for (int i = 0; i < numberofAngles + 1; i++)
+       // {
+           // float floatconvert = i;
+            //angles.Add(floatconvert / numberofAngles * 360f);
+       // }
     }
 
     // Update is called once per frame
     void Update()
     {
-            Vector3 Playerpos = Player.position;
+        //Vector3 Playerpos = Player.position;
 
-              //Draw force field
-        
-             currentIndex = (currentIndex + 1) % angles.Count; 
-            
+        //Draw force field
 
-
-             float PointA = angles[currentIndex] * Mathf.Deg2Rad;
-
-             float PointB = angles[currentIndex + 1] * Mathf.Deg2Rad;
-            
-
-            //Find vector for point A
-            //find y
-            float Ay = Mathf.Sin(PointA);
-
-            //find x 
-            float Ax = Mathf.Cos(PointA);
+        //currentIndex = (currentIndex + 1) % angles.Count;
 
 
 
-            //Find vector for point B
-            //find y
-            float By = Mathf.Sin(PointB);
+        // float PointA = angles[currentIndex] * Mathf.Deg2Rad;
 
-            //find x
-            float Bx = Mathf.Cos(PointB);
+        //float PointB = angles[currentIndex + 1] * Mathf.Deg2Rad;
 
 
-            //A spot used to be called Point
-            Vector3 PointASpot = new Vector3(Ax, Ay, 0) * radius;
+        //Find vector for point A
+        //find y
+        //float Ay = Mathf.Sin(PointA);
 
-            //B spot is experimental
-            Vector3 PointBSpot = new Vector3(Bx, By, 0) * radius;
-        
-            Debug.DrawLine(Playerpos + PointASpot, Playerpos + PointBSpot, Color.green);
-            //Debug.DrawLine(cirlecenter + PointASpot, cirlecenter + PointBSpot, Color.green);
+        //find x 
+        //float Ax = Mathf.Cos(PointA);
 
 
+
+        //Find vector for point B
+        //find y
+        //float By = Mathf.Sin(PointB);
+
+        //find x
+        //float Bx = Mathf.Cos(PointB);
+
+
+        //A spot used to be called Point
+        //Vector3 PointASpot = new Vector3(Ax, Ay, 0) * radius;
+
+        //B spot is experimental
+        //Vector3 PointBSpot = new Vector3(Bx, By, 0) * radius;
+
+        //Debug.DrawLine(Playerpos + PointASpot, Playerpos + PointBSpot, Color.green);
+        //Debug.DrawLine(cirlecenter + PointASpot, cirlecenter + PointBSpot, Color.green);
+
+
+
+        //dot product exercise
+        Vector2 redVector = VectorFromAngle(RedAngleDeg);
+        Vector2 blueVector = VectorFromAngle(BlueAngleDeg);
+
+        Debug.DrawLine(Vector3.zero, redVector, Color.red);
+        Debug.DrawLine(Vector3.zero, blueVector, Color.cyan);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float dot = DotProduct(redVector, blueVector);
+
+            if (Mathf.Abs(dot) < 0.001) dot = 0;
+            Debug.Log($"<color=yellow><size=16>{dot}</size></color>");
+        }
     }
+    
+    private float DotProduct(Vector3 a, Vector3 b)
+    {
+        return a.x * b.x + a.y * b.y;
+    }
+  
 
-
-
-
+    private Vector2 VectorFromAngle(float angle)
+    {
+        float angleinRad = Mathf.Deg2Rad * angle;
+        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
 }
