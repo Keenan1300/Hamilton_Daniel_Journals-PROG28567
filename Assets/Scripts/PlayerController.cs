6118183398608dcdf6f7a67e0a2deb0236aad553
hamilt25 <hamilt25@OA-S141-01>
2025-11-17 15:57:37 -0500

    Added player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,13 @@
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector3 velocity;
+    public float gravity;
+    public float jumpVel;
+    public LayerMask groundlayer;
+
     public enum FacingDirection
     {
         left, right
@@ -19,22 +25,42 @@ public class PlayerController : MonoBehaviour
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetButtonDown("Jump") ? 1 : 0
+        };
+
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        JumpInput(playerInput);
+        transform.position += playerInput.x * velocity * Time.deltaTime;
+    }
+
+    private void JumpInput(Vector2 playerInput)
+    {
+        if (IsGrounded() && playerInput.y == 1)
+            velocity.y = jumpVel;
+        else if (!IsGrounded())
+            velocity.y += gravity * Time.deltaTime;
+        else
+            velocity.y = 0;
 
     }
 
     public bool IsWalking()
     {
+        
         return false;
     }
     public bool IsGrounded()
     {
-        return false;
+        Vector3 origin = transform.position + Vector3.down * 0.55f;
+
+        return Physics2D.OverlapBox(origin,new Vector2 (1f,0.2f), 0, groundlayer);
     }
 
     public FacingDirection GetFacingDirection()
